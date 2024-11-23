7d3c7992acc605092caff6b146a05be6e49cb1d7
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-11-23 16:55:15 +0300

    implement marchants component

--- src/COMPONENTS/MerchantAcquisition/MerchantAcquisition.jsx
+++ src/COMPONENTS/MerchantAcquisition/MerchantAcquisition.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import './MerchantAcquisition.css'
+function MerchantAcquisition(){
+  const [merchants, setMerchants] = useState([
+    { name: "Merchant A", engagement: "High", acquisitions: 50 },
+    { name: "Merchant B", engagement: "Medium", acquisitions: 30 },
+  ]);
 
-function MerchantAcquisition() {
   return (
-    <div>MerchantAcquisition</div>
-  )
-}
+    <div className="marchantsComponent">
+      <h2>Merchant Acquisition</h2>
+      <ul>
+        {merchants.map((merchant, i) => (
+          <li key={i}>
+            <strong>{merchant.name}</strong>: :{merchant.engagement} engagement, {merchant.acquisitions} acquisitions
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default MerchantAcquisition
+export default MerchantAcquisition;
